6c1914b1b09e725d97c47acfbb0db36ceb83b8c3
PacoAlexM <pako_ale_martell@hotmail.com>
2025-11-24 22:39:19 -0600

    Se agregan secciones de control de flujos

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,13 +1,19 @@
 import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+type Grade = 'S'|'A'|'B'|'C'|'D'
 
 @Component({
     selector: 'control-flow',
-    imports: [],
+    imports: [TitleComponent],
     templateUrl: './control-flow.component.html',
 })
 export default class controlFlowComponent {
     public showContent = signal(false);
-
+    public grade = signal<Grade>('A');
+    public frameworks = signal(['Angular', 'React', 'Vue', 'Qwik', 'Express']);
+    public frameworks2 = signal([]);
+    
     public toggleContent() {
         this.showContent.update(val => !val);
     }
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'title',
+    selector: 'my-title',
     imports: [],
-    templateUrl: './title.component.html',
+    template: `
+        <h1 class="text-3xl mb-5">{{ title }}</h1>
+    `,
 })
-export class TitleComponent {}
+export class TitleComponent {
+    // title = input.required<string>();
+    @Input({ required: true }) title!: string;
+    @Input({ transform: booleanAttribute }) withShadow: boolean = false;
+}
